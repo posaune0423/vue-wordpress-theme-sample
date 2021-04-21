cbec4897ab8e83b219b97212b311f09e7cc04a79
Asuma Yamada <asuma@linear-jp.biz>
2021-04-22 01:43:56 +0900

    fix: modify index.php

--- wp-content/themes/sample-theme/index.php
+++ wp-content/themes/sample-theme/index.php
@@ -1,5 +1,20 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
-<div id="app"></div>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta name="theme-color" content="#42b983">
+	<?php if (is_front_page()) : ?>
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+</head>
 
-<?php get_footer();
+<body <?php body_class(); ?>>
+	<div id="app"></div>
+	<?php wp_footer(); ?>
+</body>
+
+</html>
