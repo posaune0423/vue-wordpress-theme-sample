bf5c56937b241e56e7075ed3765568a41a0dc855
Asuma Yamada <asuma@linear-jp.biz>
2021-07-16 02:13:38 +0900

    fix: modify component

--- wp-content/themes/sample-theme/src/components/HelloWorld.vue
+++ wp-content/themes/sample-theme/src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-	<v-btn>Test</v-btn>
+    <v-btn>Test</v-btn>
+    <div class="test">
+      <a href="">oooooooooooooooo</a>
+    </div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -97,7 +100,14 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+.test {
+  a {
+    &:hover {
+      color: red;
+    }
+  }
+}
 h1 {
   color: red;
 }
