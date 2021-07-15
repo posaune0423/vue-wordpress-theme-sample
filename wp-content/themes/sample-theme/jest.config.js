6ab9208ac7a2a3fecafbb71e64b3e990a7cd2d44
Asuma Yamada <asuma@linear-jp.biz>
2021-07-15 20:21:24 +0900

    feat: add jest config file

---
+++ wp-content/themes/sample-theme/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!./tests/**',
+    '!**/node_modules/**'
+  ],
+  setupFiles: ['./tests/unit/setup.js']
+};
