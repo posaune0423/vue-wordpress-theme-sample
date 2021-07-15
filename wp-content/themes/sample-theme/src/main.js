5e92de66d41f9450e168a3be6ccd758855596114
Asuma Yamada <asuma@linear-jp.biz>
2021-07-16 02:13:23 +0900

    fix: modify import scss

--- wp-content/themes/sample-theme/src/main.js
+++ wp-content/themes/sample-theme/src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
-// require('@/assets/stylesheets/normalize.scss');
+require('@/assets/styles/base.scss');
 
 Vue.config.productionTip = false;
 
