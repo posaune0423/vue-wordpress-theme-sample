43790e0f0ab9e9cbd7846053f323dd4c86a9a997
Asuma Yamada <asuma@linear-jp.biz>
2021-07-17 05:35:00 +0900

    fix: configuration of axios

--- sample-theme/src/plugins/axios.js
+++ sample-theme/src/plugins/axios.js
@@ -1,2 +1,4 @@
 import axios from 'axios';
-export default axios.create({baseURL = process.env.API_BASE_URL || 'http://localhost:8000/'});
+export default axios.create({
+  baseURL: process.env.API_BASE_URL || 'http://localhost:8000/wp-json/wp/v2/'
+});
