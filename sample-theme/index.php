e87b853b72452d61ae07e7afff6c118af9fe1d39
Asuma Yamada <asuma@linear-jp.biz>
2021-07-17 05:36:17 +0900

    format: file

--- sample-theme/index.php
+++ sample-theme/index.php
@@ -2,20 +2,20 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
-	<meta name="theme-color" content="#42b983">
-	<?php if (is_front_page()) : ?>
-		<meta name="description" content="<?php bloginfo('description'); ?>">
-	<?php endif; ?>
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="icon" href="<%= BASE_URL %>logo.svg">
+    <meta name="theme-color" content="#42b983">
+    <?php if (is_front_page()) : ?>
+        <meta name="description" content="<?php bloginfo('description'); ?>">
+    <?php endif; ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="app"></div>
-	<?php wp_footer(); ?>
+    <div id="app"></div>
+    <?php wp_footer(); ?>
 </body>
 
 </html>
