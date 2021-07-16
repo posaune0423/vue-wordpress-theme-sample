51b92b74174b483cf41274b8de61a3156ca6628b
Asuma Yamada <asuma@linear-jp.biz>
2021-07-17 03:36:24 +0900

    fix: configure axios

--- sample-theme/src/main.js
+++ sample-theme/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import axios from './plugins/axios';
+
 
 require('@/assets/styles/base.scss');
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
---
+++ sample-theme/src/plugins/axios.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+export default axios.create({baseURL = process.env.API_BASE_URL || 'http://localhost:8000/'});
